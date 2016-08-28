67e17666733a51664a8cafab2c967b23c0fc2de2
marya314 <mary.boyd314@gmail.com>
2016-08-27 22:28:22 -0400

    Added authentication code to sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
+        if @user.save
+            redirect_to users_path
+        else
+            render 'new'
+        end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :walks
   has_many :organisms, through: :walks
+
+  validates :password, presence: true, :on => :create
+
 end
